0807f74574a1930fde5da6ca7f61e87567cab657
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-12 21:24:05 +0300

    Reimplement the item class to fit for other child

--- lib/item.rb
+++ lib/item.rb
@@ -1,21 +1,27 @@
-require 'date'
+require "date"
 
 class Item
   attr_accessor :genre, :author, :label, :publish_date
   attr_reader :id, :archived
 
-  def initialize(genre, author, label, publish_date, archived)
-    @id = Random.rand(1..10_000)
+  def initialize(publish_date, archived, id: Random.rand(1..1000))
+    @id = id
     @publish_date = publish_date
     @archived = archived
+    @author = nil
+    @genre = nil
+    @label = nil
+  end
 
-    @genre = genre
-    genre.add_item(self)
-
-    @author = author
+  def add_author(author)
     author.add_item(self)
+  end
 
-    @label = label
+  def add_genre(genre)
+    genre.add_item(self)
+  end
+
+  def add_label(label)
     label.add_item(self)
   end
 
