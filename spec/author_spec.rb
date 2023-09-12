0a618e9b17b929c0c82f785f54b4fc1d9534d551
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-13 01:01:14 +0300

    test adds the item to the author class

---
+++ spec/author_spec.rb
@@ -0,0 +1,16 @@
+require_relative "../lib/classes/author"
+require_relative "../lib/item"
+
+RSpec.describe Author do
+  describe "#add_item" do
+    it "adds the item to the author" do
+      author = Author.new("Ahmed", "Eid")
+      item = Item.new("01/01/2019", false)
+
+      author.add_item(item)
+
+      expect(author.items).to include(item)
+      expect(item.author).to eq(author)
+    end
+  end
+end
