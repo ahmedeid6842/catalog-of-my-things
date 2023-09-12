94e0364152d68992af1a2ed34af0e358de6cc1d0
Mathías Wismann <mwbertolucci@gmail.com>
2023-09-12 17:01:36 -0400

    Add data directory and Book class model

---
+++ lib/models/book.rb
@@ -0,0 +1,17 @@
+require_relative '../lib/item'
+
+class Book < Item
+  attr_accessor :title, :cover_state
+  attr_reader :publisher
+
+  def initialize(title, publisher, cover_state, *args)
+    super(*args)
+    @title = title
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    super || @cover_state == 'bad'
+  end
+end
