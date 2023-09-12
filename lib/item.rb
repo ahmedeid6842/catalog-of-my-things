e5bbdce28963b443d660f7a1bdee427791fc1775
Reza Merzaie <ghrezam97@gmail.com>
2023-09-13 00:02:36 +0430

    Refactor the Item class

--- lib/item.rb
+++ lib/item.rb
@@ -4,24 +4,20 @@ class Item
   attr_accessor :genre, :author, :label, :publish_date
   attr_reader :id, :archived
 
-  def initialize(genre, author, label, publish_date, archived)
+  def initialize(publish_date)
     @id = Random.rand(1..10_000)
     @publish_date = publish_date
+    @genre = nil
     @archived = archived
-
-    @genre = genre
-    genre.add_item(self)
-
-    @author = author
-    author.add_item(self)
-
-    @label = label
-    label.add_item(self)
   end
 
   def can_be_archived?
     date_10_years_ago = Date.today - (10 * 365)
     true unless @publish_date > date_10_years_ago
+  end
+
+  def add_genre(genre)
+    genre.add_item(self)
   end
 
   def move_to_archive
