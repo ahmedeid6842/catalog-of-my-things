ac75e99640fe2da70d31985464b4d3c231eba642
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-12 21:25:33 +0300

    Added the Game class attributes

--- lib/classes/game.rb
+++ lib/classes/game.rb
@@ -1,6 +1,11 @@
 require_relative "../item"
 
 class Game < Item
-  def initialize()
+  attr_accessor :multiplayer, :last_played_at
+
+  def initialize(multiplayer, last_played_at, publish_date)
+    super(publish_date, archived)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
   end
 end
