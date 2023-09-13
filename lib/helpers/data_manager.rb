dab5811020a2b361953000ecbab38d154e0ae918
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-13 23:30:07 +0300

    Make the serialize_data open for extensions for diffrerent items

--- lib/helpers/data_manager.rb
+++ lib/helpers/data_manager.rb
@@ -1,4 +1,4 @@
-require 'json'
+require "json"
 
 class DataManager
   def initialize(items)
@@ -7,36 +7,47 @@ class DataManager
 
   def save_data(items)
     items_data = serialize_data(items)
-    File.write('./lib/records/items.json', JSON.generate(items_data))
+    File.write("./lib/records/items.json", JSON.generate(items_data))
   end
 
   def load_data
-    items_data = File.exist?('./lib/records/items.json') ? JSON.parse(File.read('./lib/records/items.json')) : []
+    items_data = File.exist?("./lib/records/items.json") ? JSON.parse(File.read("./lib/records/items.json")) : []
     deserialize_data(items_data)
   end
 
   def serialize_data(items)
     items.map do |item|
-      next unless item.instance_of?(MusicAlbum)
-
-      {
-        type: 'MusicAlbum',
-        id: item.id,
-        genre_name: item.genre.name,
-        publish_date: item.publish_date,
-        on_spotify: item.on_spotify
-      }
+      if item.instance_of?(MusicAlbum)
+        {
+          type: "MusicAlbum",
+          id: item.id,
+          genre_name: item.genre.name,
+          publish_date: item.publish_date,
+          on_spotify: item.on_spotify,
+        }
+      elsif item.instance_of?(Game)
+        {
+          type: "Game",
+          id: item.id,
+          multiplayer: item.multiplayer,
+          last_played_at: item.last_played_at,
+          publish_date: item.publish_date,
+          author: "#{item.author.first_name} #{item.author.last_name}",
+        }
+      else
+        next
+      end
     end
   end
 
   def deserialize_data(items_data)
     items = []
     items_data.each do |item_data|
-      next unless item_data['type'] == 'MusicAlbum'
+      next unless item_data["type"] == "MusicAlbum"
 
-      genre_name = item_data['genre_name']
-      publish_date = item_data['publish_date'].to_i
-      on_spotify = item_data['on_spotify']
+      genre_name = item_data["genre_name"]
+      publish_date = item_data["publish_date"].to_i
+      on_spotify = item_data["on_spotify"]
 
       genre = Genre.new(genre_name)
       music_album = MusicAlbum.new(genre, publish_date, on_spotify)
